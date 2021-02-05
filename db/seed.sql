ab6e9a7842d332a5cb659edc4fc921cb2dfc6484
Alyson May <amay1421@yahoo.com>
2021-02-04 22:11:11 -0600

    Commit from tutor sesesh, updated SQL table and connected a few functions

--- db/seed.sql
+++ db/seed.sql
@@ -1,36 +1,10 @@
-CREATE DATABASE employee_trackerDB;
+USE employee_trackerDB
 
-USE employee_trackerDB;
+INSERT INTO Department (name)
+VALUES ("Engineering"), ("HR")("Engineering"),;
 
-CREATE Department  (
-  department_id INT NOT NULL,
-  name VARCHAR(30),
-  PRIMARY KEY (id)
-);
+INSERT INTO Role (title, salary, department_id)
+VALUES ("test tile", 40000, 1);
 
-CREATE Role  (
-  role_id INT NOT NULL,
-  title VARCHAR(30),
-  salary DECIMAL(10,2) NULL,
-  department_id INT NULL,
-  PRIMARY KEY (role_id)
-);
-
-CREATE Employee  (
-  id INT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
-
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100);
-
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("chocolate", 3.10, 120);
-
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("strawberry", 3.25, 75);
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, 1);
---
+++ server.js
@@ -0,0 +1,180 @@
+const inquirer = require('inquirer')
+const fs = require('fs');
+
+require("console.table");
+
+const mysql = require('mysql');
+const { errorMonitor } = require('events');
+const { error } = require('console');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "rootroot",
+    database: "employee_trackerDB"
+})
+
+
+// Main Menu Question and Switch
+const main = () => {
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "main",
+                message: "What would you like to do?",
+                choices: ["View all employees", "View employees by department", "View employees by role", "Add a new employee", "Add a new department", "Add a new role", "Update employee roles",]
+            }
+        ]).then((answer) => {
+            switch (answer.main) {
+                case "View all employees": getEmployeeInformation()
+                    break;
+                case "Department": getDepartmentInformation()
+                    break;
+                case "employeeRoles": getEmployeeRolesInfo()
+                    break;
+                case "addEmployee": getNewEmployeeInfo()
+                    break;
+                case "Add a new department": getNewDepartmentInfo()
+                    break;
+                case "newRole": getNewRoleInfo()
+                    break;
+                case "updateRoles": getUpdateRoleInfo()
+                    break;
+                case "Exit": getSQL()
+                    break;
+            }
+        })
+
+    }
+
+    // View All Employees
+    const getEmployeeInformation = () => {
+        // SQL Database?
+
+        connection.query("SELECT * FROM Employee", (err, queryResult) => {
+
+            console.log(err)
+            console.table(queryResult);
+
+            main();
+        })
+    }
+
+    //View Employees By Department
+    const getDepartmentInformation = () => {
+        // SQL Database?
+    }
+
+    // View Employees By Roles
+    const getEmployeeRolesInfo = () => {
+        // SQL Database?
+    }
+
+    // Add New Employee
+    const getNewEmployeeInfo = () => {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "FN",
+                    message: "What is the firt name of the employee?",
+                },
+                {
+                    type: "input",
+                    name: "LN",
+                    message: "What is the last name of the employee?",
+                },
+                {
+                    type: "input",
+                    name: "title",
+                    message: "What is the their title?",
+                },
+                {
+                    type: "input",
+                    name: "department",
+                    message: "What department are they in?",
+                },
+                {
+                    type: "input",
+                    name: "manager",
+                    message: "Who is their manager?",
+                },
+            ]).then((answers) => {
+                // const newEmployee = new Employee(answers.FN, answers.LN, answers.title, answers.department, answers.manager)
+                // team.push(newEngineer)
+                // employeeType()
+                
+            })
+    }
+
+    // Add New Department
+    const getNewDepartmentInfo = () => {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "newDepartment",
+                    message: "What is the name of the new department?",
+                }
+            ]).then((answers) => {
+                // const newDepartment = new Department(answers.newDepartment)
+                // team.push(newEngineer)
+                // employeeType()
+
+                connection.query(`INSERT INTO Department (name) VALUES ("${answers.newDepartment}")`, (err, queryResult) => {
+
+                    console.log(err)
+                    console.table(queryResult);
+        
+                    main();
+                })
+
+            })
+    }
+
+    // Add New Role
+    const getNewRoleInfo = () => {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "newRole",
+                    message: "What is the name of the new role?",
+                }
+            ]).then((answers) => {
+                const newRole = new Role(answers.newRole)
+                // team.push(newEngineer)
+                // employeeType()
+            })
+    }
+
+    // Update A Role
+    const getUpdateRoleInfo = () => {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "updateRole",
+                    message: "What role would you like to update?",
+                }
+
+            ]).then((answers) => {
+                const updateRole = new updateRole(answers.updateRole)
+                // team.push(newEngineer)
+                // employeeType()
+            })
+
+    }
+
+
+
+    connection.connect(() => {
+        console.log('Successfully connected to database!')
+        main ();
+    })
